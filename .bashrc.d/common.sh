8197d42a5f66c45fa0c2761cbf96000479288dac
mijime <07cc0110+gthb@gmail.com>
2015-11-25 09:19:51 +0900

    Gitの情報はcdしたタイミングで取得するように

--- .bashrc.d/common.sh
+++ .bashrc.d/common.sh
@@ -35,12 +35,38 @@ custom_prompt(){
   [[ 0 -eq ${__shell_result} ]] &&
     __shell_result_ps1="\[\e[0;32m\][${__shell_result}]" ||
     __shell_result_ps1="\[\e[0;31m\][${__shell_result}]"
-  [[ -z ${__no_git_prompt} ]] && {
-    __git_ps1="\[\e[0;31m\]$(__git_info_ps1)"
-  } || {
-    unset __git_ps1
-  }
+  [[ -z ${__no_git_prompt} ]] || unset __git_ps1
   PS1="\[\e]0;\w\a\]${__user_info_ps1} \[\e[33m\]\w ${__shell_result_ps1}${__git_ps1}\n\[\e[0m\]\$ "
 }
 
 PROMPT_COMMAND='custom_prompt'
+
+cd_func(){
+  [[ $# -eq 0 ]] && popd > /dev/null
+
+  while [[ $# -gt 0 ]]
+  do
+    case "$1" in
+      -s|--save)
+        dirs -v -l | awk '{print"pushd -n \""$2"\";"}'
+        shift || break
+        ;;
+      -c|--clear)
+        dirs -c
+        shift || break
+        ;;
+      -l|--list)
+        dirs -v -p | awk -v dispf="%3d %s\n" '!nl[$2]{nl[$2]=$1}END{for(k in nl)printf dispf,nl[k],k}' | sort -n
+        shift || break
+        ;;
+      *)
+        pushd "$1" > /dev/null
+        __git_ps1="\[\e[0;31m\]$(__git_info_ps1)"
+        shift || break
+        ;;
+    esac
+  done
+}
+
+alias cd='cd_func'
+alias pd='popd'
