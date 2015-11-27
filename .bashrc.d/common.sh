7d24925a1cfadcc318aeb25eeb5f3c6174be7795
mijime <07cc0110+gthb@gmail.com>
2015-11-27 16:26:40 +0900

    prefix をおしゃんティーに

--- .bashrc.d/common.sh
+++ .bashrc.d/common.sh
@@ -1,66 +1,50 @@
 #!/bin/bash
 
-# [history]
-__share_history(){
+mysh:history:share(){
   history -a
   history -r
 }
 
-case ${SHELL} in
-  *bash)
-    shopt -u histappend
-    export HISTSIZE=9999
-    export HISTTIMEFORMAT='%y/%m/%d %H:%M:%S  '
-    export HISTIGNORE='l[sla]:history:pwd:exit:cd:[bf]g:jobs'
-    export HISTCONTROL='ignoredups:ignorespace:erasedups'
-    ;;
-  *zsh)
-    HISTFILE=~/.zsh_history
-    HISTSIZE=10000
-    SAVEHIST=10000
-    setopt hist_ignore_dups
-    setopt share_history
-    ;;
-esac
-
-# [prompt]
-__git_info_ps1(){
+mysh:prompt:git(){
   git status --porcelain --branch 2>/dev/null |
   awk '/^##/{branch=$2}END{if(NR>0){print" ("branch,NR-1")"}}'
 }
-__user_info_ps1="\[\e[36m\]\u\[\e[0m\]@\[\e[34m\]\h"
 
-custom_prompt(){
+mysh:prompt(){
   __shell_result=$?
   [[ 0 -eq ${__shell_result} ]] &&
-    __shell_result_ps1="\[\e[0;32m\][${__shell_result}]" ||
-    __shell_result_ps1="\[\e[0;31m\][${__shell_result}]"
-  [[ -z ${__no_git_prompt} ]] || unset __git_ps1
-  PS1="\[\e]0;\w\a\]${__user_info_ps1} \[\e[33m\]\w ${__shell_result_ps1}${__git_ps1}\n\[\e[0m\]\$ "
+    __ps1_shell_result="\[\e[0;32m\][${__shell_result}]" ||
+    __ps1_shell_result="\[\e[0;31m\][${__shell_result}]"
+  [[ -z ${__ps1_git_ignore} ]] || unset __ps1_git
+  PS1="\[\e]0;\w\a\]\[\e[36m\]\u\[\e[0m\]@\[\e[34m\]\h \[\e[33m\]\w ${__ps1_shell_result}${__ps1_git}\n\[\e[0m\]\$ "
 }
 
-PROMPT_COMMAND='custom_prompt'
+mysh:update(){
+  mysh:history:share
+  __ps1_git="\[\e[0;31m\]$(mysh:prompt:git)"
+}
 
-__interactiveGrep(){
+mysh:filter(){
   commands=$(echo ${FILTER_COMMAND:-fzf:grep}|sed 's/:/ /g')
   for cmd in ${commands[@]}
   do
     hash ${cmd} 1>/dev/null 2>/dev/null || continue
 
     case ${cmd} in
-      fzf|peco)
+      fzf)
+        ${cmd} --extended --query="$1"
+        return ;;
+      peco)
         ${cmd} --query="$1"
-        return
-        ;;
+        return ;;
       *)
         ${cmd} "$1"
-        return
-        ;;
+        return ;;
     esac
   done
 }
 
-__cdFunc(){
+mysh:cd(){
   [[ $# -eq 0 ]] && { pushd || pushd $HOME; } 1>/dev/null 2>/dev/null
 
   while [[ $# -gt 0 ]]
@@ -76,7 +60,6 @@ __cdFunc(){
         ;;
       [-+][0-9]*)
         pushd "$1" > /dev/null
-        __git_ps1="\[\e[0;31m\]$(__git_info_ps1)"
         shift || break
         ;;
       -*)
@@ -84,14 +67,32 @@ __cdFunc(){
         break
         ;;
       *)
-        [[ -d "$1" ]] && targetDir="$1" || targetDir=$(find . -type d|__interactiveGrep "$1"|head -1||break)
+        [[ -d "$1" ]] && targetDir="$1" || targetDir=$(find . -type d|mysh:filter "$1"|head -1||break)
         pushd "${targetDir}" > /dev/null
-        __git_ps1="\[\e[0;31m\]$(__git_info_ps1)"
         shift || break
         ;;
     esac
   done
+  mysh:update
 }
 
-alias cd='__cdFunc'
+alias cd='mysh:cd'
 alias pd='popd'
+PROMPT_COMMAND='mysh:prompt'
+
+case ${SHELL} in
+  *bash)
+    # shopt -u histappend
+    export HISTSIZE=9999
+    export HISTTIMEFORMAT='[%y/%m/%d %H:%M:%S]  '
+    export HISTIGNORE='l[sla]:history*:pwd:exit:cd:[bf]g:jobs'
+    export HISTCONTROL='ignoredups:ignorespace:erasedups'
+    ;;
+  *zsh)
+    HISTFILE=~/.zsh_history
+    HISTSIZE=9999
+    SAVEHIST=9999
+    setopt hist_ignore_dups
+    setopt history:share
+    ;;
+esac
