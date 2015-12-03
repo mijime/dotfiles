342e9b71b80043b76125b5f599399f50a56be236
mijime <07cc0110+gthb@gmail.com>
2015-12-03 14:08:22 +0900

    [bashrc.d] Reformat

--- .bashrc.d/common.sh
+++ .bashrc.d/common.sh
@@ -5,13 +5,13 @@ echo "\033[3${1}m"
 }
 
 mysh::color::prefix::zsh(){
-echo "%F{$(($1 + 1))}"
+echo "%F{$((${1} + 1))}"
 }
 
 mysh::prompt::bash(){
+
 __myshPromptInfoBash="${__myshColo[6]}\\u${__myshColo[7]}@${__myshColo[4]}\\h ${__myshColo[3]}[\\w]"
 PROMPT_COMMAND=mysh::prompt::update
-
 shopt -u histappend
 HISTSIZE=9999
 HISTTIMEFORMAT='[%y/%m/%d %H:%M:%S]  '
@@ -20,9 +20,9 @@ HISTCONTROL='ignoredups:ignorespace:erasedups'
 }
 
 mysh::prompt::zsh(){
+
 __myshPromptInfoZsh="${__myshColo[6]}%n${__myshColo[7]}@${__myshColo[4]}%m ${__myshColo[3]}[%~]"
 precmd_functions=(mysh::prompt::update)
-
 setopt hist_ignore_dups
 HISTFILE=~/.zsh_history
 HISTSIZE=9999
@@ -42,6 +42,7 @@ RPROMPT="${__myshPromptGit}${__myshPromptInfoZsh}"
 }
 
 mysh::prompt::git(){
+
 git status --porcelain --branch 2>/dev/null |
 awk '/^##/{branch=$2}END{if(NR>0){print"("branch,NR-1") "}}'
 }
@@ -60,31 +61,33 @@ mysh::prompt::update::${__mysh}
 
 mysh::filter(){
 
-commands=($(echo ${FILTER_COMMAND:-fzf:grep}|sed 's/:/ /g'))
-for cmd in ${commands[@]}
+local __myshCommands=($(echo ${FILTER_COMMAND:-fzf:grep}|sed 's/:/ /g'))
+
+for cmd in ${__myshCommands[@]}
 do
   hash ${cmd} 1>/dev/null 2>/dev/null || continue
 
   case ${cmd} in
     fzf)
-      ${cmd} --extended --query="$1"
+      ${cmd} --extended --query="${1}"
       return ;;
     peco)
-      ${cmd} --layout bottom-up --query="$1"
+      ${cmd} --layout bottom-up --query="${1}"
       return ;;
     *)
-      ${cmd} "$1"
+      ${cmd} "${1}"
       return ;;
   esac
 done
 }
 
 mysh::cd(){
-[[ $# -eq 0 ]] && { pushd || pushd $HOME; } 1>/dev/null 2>/dev/null
+
+[[ $# -eq 0 ]] && { pushd || pushd ${HOME}; } 1>/dev/null 2>/dev/null
 
 while [[ $# -gt 0 ]]
 do
-  case "$1" in
+  case "${1}" in
     -c|--clear)
       dirs -c
       break
@@ -94,7 +97,7 @@ do
       break
       ;;
     [-+][0-9]*)
-      pushd "$1" > /dev/null
+      pushd "${1}" > /dev/null
       shift || break
       ;;
     -*)
@@ -102,7 +105,7 @@ do
       break
       ;;
     *)
-      [[ -d "$1" ]] && targetDir="$1" || targetDir=$(find . -type d -or -type l|mysh::filter "$1"|head -1||break)
+      [[ -d "${1}" ]] && targetDir="${1}" || targetDir=$(find . -type d -or -type l|mysh::filter "${1}"|head -1||break)
       pushd "${targetDir}" > /dev/null
       shift || break
       ;;
@@ -111,7 +114,7 @@ done
 }
 
 __mysh=${SHELL##*/}
-__myshColo=($(seq 0 7|while read n; do mysh::color::prefix::${__mysh} $n; done))
+__myshColo=($(seq 0 7|while read n; do mysh::color::prefix::${__mysh} ${n}; done))
 mysh::prompt::${__mysh}
 
 alias cd='mysh::cd'
