0e9c4c8253c3e3a5c37dd40eae047d6df6056774
mijime <07cc0110+gthb@gmail.com>
2015-12-03 13:07:48 +0900

    [bashrc.d] Summarized a functions as "mysh"

--- .bashrc.d/common.sh
+++ .bashrc.d/common.sh
@@ -1,98 +1,118 @@
 #!/bin/bash
 
-mysh:history:share(){
-  history -a
-  history -r
+mysh::color::prefix::bash(){
+echo "\033[3${1}m"
 }
 
-mysh:prompt:git(){
-  git status --porcelain --branch 2>/dev/null |
-  awk '/^##/{branch=$2}END{if(NR>0){print" ("branch,NR-1")"}}'
+mysh::color::prefix::zsh(){
+echo "%F{$(($1 + 1))}"
 }
 
-mysh:prompt(){
-  __shell_result=$?
-  [[ 0 -eq ${__shell_result} ]] &&
-    __ps1_shell_result="\[\e[0;32m\][${__shell_result}]" ||
-    __ps1_shell_result="\[\e[0;31m\][${__shell_result}]"
-  [[ -z ${__ps1_git_ignore} ]] || unset __ps1_git
-  PS1="\[\e]0;\w\a\]\[\e[36m\]\u\[\e[0m\]@\[\e[34m\]\h \[\e[33m\]\w ${__ps1_shell_result}${__ps1_git}\n\[\e[0m\]\$ "
+mysh::prompt::bash(){
+__myshPromptInfoBash="${__myshColo[6]}\\u${__myshColo[7]}@${__myshColo[4]}\\h ${__myshColo[3]}[\\w]"
+PROMPT_COMMAND=mysh::prompt::update
+
+shopt -u histappend
+HISTSIZE=9999
+HISTTIMEFORMAT='[%y/%m/%d %H:%M:%S]  '
+HISTIGNORE='l[sla]:history*:pwd:exit:cd:[bf]g:jobs'
+HISTCONTROL='ignoredups:ignorespace:erasedups'
 }
 
-mysh:update(){
-  mysh:history:share
-  __ps1_git="\[\e[0;31m\]$(mysh:prompt:git)"
+mysh::prompt::zsh(){
+__myshPromptInfoZsh="${__myshColo[6]}%n${__myshColo[7]}@${__myshColo[4]}%m ${__myshColo[3]}[%~]"
+precmd_functions=(mysh::prompt::update)
+
+setopt hist_ignore_dups
+HISTFILE=~/.zsh_history
+HISTSIZE=9999
+SAVEHIST=9999
 }
 
-mysh:filter(){
-  commands=$(echo ${FILTER_COMMAND:-fzf:grep}|sed 's/:/ /g')
-  for cmd in ${commands[@]}
-  do
-    hash ${cmd} 1>/dev/null 2>/dev/null || continue
+mysh::prompt::update::bash(){
 
-    case ${cmd} in
-      fzf)
-        ${cmd} --extended --query="$1"
-        return ;;
-      peco)
-        ${cmd} --layout bottom-up --query="$1"
-        return ;;
-      *)
-        ${cmd} "$1"
-        return ;;
-    esac
-  done
+PS1="${__myshPromptInfoBash}
+${__myshPromptGit}${__myshPromptCodeColor}$\033[00m "
 }
 
-mysh:cd(){
-  [[ $# -eq 0 ]] && { pushd || pushd $HOME; } 1>/dev/null 2>/dev/null
+mysh::prompt::update::zsh(){
 
-  while [[ $# -gt 0 ]]
-  do
-    case "$1" in
-      -c|--clear)
-        dirs -c
-        break
-        ;;
-      -l|--list|--history)
-        dirs -l -v | awk '!nl[$2]{print;nl[$2]=1}'
-        break
-        ;;
-      [-+][0-9]*)
-        pushd "$1" > /dev/null
-        shift || break
-        ;;
-      -*)
-        pushd $@
-        break
-        ;;
-      *)
-        [[ -d "$1" ]] && targetDir="$1" || targetDir=$(find . -type d -or -type l|mysh:filter "$1"|head -1||break)
-        pushd "${targetDir}" > /dev/null
-        shift || break
-        ;;
-    esac
-  done
-  mysh:update
+PROMPT="${__myshPromptCodeColor}$%f "
+RPROMPT="${__myshPromptGit}${__myshPromptInfoZsh}"
 }
 
-alias cd='mysh:cd'
+mysh::prompt::git(){
+git status --porcelain --branch 2>/dev/null |
+awk '/^##/{branch=$2}END{if(NR>0){print"("branch,NR-1") "}}'
+}
+
+mysh::prompt::update(){
+
+__myshPromptCode=$?
+[[ 0 -eq ${__myshPromptCode} ]] &&
+  __myshPromptCodeColor="${__myshColo[6]}" ||
+  __myshPromptCodeColor="${__myshColo[1]}"
+
+__myshPromptGit="${__myshColo[1]}$(mysh::prompt::git)"
+
+mysh::prompt::update::${__mysh}
+}
+
+mysh::filter(){
+
+commands=($(echo ${FILTER_COMMAND:-fzf:grep}|sed 's/:/ /g'))
+for cmd in ${commands[@]}
+do
+  hash ${cmd} 1>/dev/null 2>/dev/null || continue
+
+  case ${cmd} in
+    fzf)
+      ${cmd} --extended --query="$1"
+      return ;;
+    peco)
+      ${cmd} --layout bottom-up --query="$1"
+      return ;;
+    *)
+      ${cmd} "$1"
+      return ;;
+  esac
+done
+}
+
+mysh::cd(){
+[[ $# -eq 0 ]] && { pushd || pushd $HOME; } 1>/dev/null 2>/dev/null
+
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    -c|--clear)
+      dirs -c
+      break
+      ;;
+    -l|--list|--history)
+      dirs -l -v | awk '!nl[$2]{print;nl[$2]=1}'
+      break
+      ;;
+    [-+][0-9]*)
+      pushd "$1" > /dev/null
+      shift || break
+      ;;
+    -*)
+      pushd $@
+      break
+      ;;
+    *)
+      [[ -d "$1" ]] && targetDir="$1" || targetDir=$(find . -type d -or -type l|mysh::filter "$1"|head -1||break)
+      pushd "${targetDir}" > /dev/null
+      shift || break
+      ;;
+  esac
+done
+}
+
+__mysh=${SHELL##*/}
+__myshColo=($(seq 0 7|while read n; do mysh::color::prefix::${__mysh} $n; done))
+mysh::prompt::${__mysh}
+
+alias cd='mysh::cd'
 alias pd='popd'
-PROMPT_COMMAND='mysh:prompt'
-
-case ${SHELL} in
-  *bash)
-    # shopt -u histappend
-    export HISTSIZE=9999
-    export HISTTIMEFORMAT='[%y/%m/%d %H:%M:%S]  '
-    export HISTIGNORE='l[sla]:history*:pwd:exit:cd:[bf]g:jobs'
-    export HISTCONTROL='ignoredups:ignorespace:erasedups'
-    ;;
-  *zsh)
-    HISTFILE=~/.zsh_history
-    HISTSIZE=9999
-    SAVEHIST=9999
-    setopt hist_ignore_dups
-    setopt history:share
-    ;;
-esac
