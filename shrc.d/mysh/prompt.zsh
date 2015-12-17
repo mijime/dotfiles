79d750095542454456c664c8a9ba85d64dbeca96
mijime <07cc0110+gthb@gmail.com>
2015-12-17 10:54:16 +0900

    :art: Add history option

--- shrc.d/mysh/prompt.zsh
+++ shrc.d/mysh/prompt.zsh
@@ -24,7 +24,15 @@ mysh_prompt() {
 
   precmd_functions=(mysh_prompt_update)
   setopt EXTENDED_HISTORY
+  setopt hist_expand
+  setopt hist_ignore_all_dups
   setopt hist_ignore_dups
+  setopt hist_ignore_space
+  setopt hist_no_store
+  setopt hist_reduce_blanks
+  setopt hist_save_no_dups
+  setopt hist_verify
+  setopt inc_append_history
   setopt share_history
   bindkey -e
   bindkey "^N" history-beginning-search-backward
