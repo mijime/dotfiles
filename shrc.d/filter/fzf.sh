c6c269f7210229128a8c126e4e819e4b88f04e72
mijime <07cc0110+gthb@gmail.com>
2016-09-15 00:12:29 +0900

    :+1: Add fzf default command

---
+++ shrc.d/filter/fzf.sh
@@ -0,0 +1,3 @@
+export FZF_IGNORE_FILTER="/\.git/\|/\.svn/\|/\.\?te\?mp/\|/\.\?cache/\|/logs\?/\|/node_modules/"
+export FZF_DEFAULT_COMMAND="{ git ls-files || ghq list --full-path & find . | grep -v \"${FZF_IGNORE_FILTER}\"; } 2>/dev/null"
+export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
