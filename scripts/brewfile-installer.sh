b1020ed81b73ba9c63f4420a45bc764aad7bed3d
mijime <07cc0110+gthb@gmail.com>
2019-02-15 00:01:47 +0900

    Update

--- scripts/brewfile-installer.sh
+++ scripts/brewfile-installer.sh
@@ -4,23 +4,22 @@ set -ue
 
 
 __noinstalled_package() {
-  local brew_cmd=${@:-brew}
+  local brew_cmd=${*:-brew}
   {
     ${brew_cmd} list | awk '{print"brewlist",$0}'
-    cat /dev/stdin | sed -e 's|#.*||' | awk '$1!=""{print"brewfile",$0}'
+    sed -e 's|#.*||' < /dev/stdin | awk '$1!=""{print"brewfile",$0}'
   } \
     | awk '$1=="brewlist"{p[$2]=1}$1=="brewfile"&&p[$2]!=1{print}' \
     | sed -e 's/brewfile//'
 }
 
-cat $(dirname $0)/brewfile | __noinstalled_package | while read package
-do brew install ${package}
+__noinstalled_package < "$(dirname "$0")/brewfile" | while read -r package
+do brew install "${package}"
 done
 brew upgrade
-brew cleanup
 
-cat $(dirname $0)/brewfile-cask | __noinstalled_package brew cask | while read package
-do brew cask install ${package}
+__noinstalled_package brew cask < "$(dirname "$0")/brewfile-cask" | while read -r package
+do brew cask install "${package}"
 done
 brew cask upgrade
 
