442d84d228070b296a3f6de29056b20ab43dd8ef
mijime <07cc0110+gthb@gmail.com>
2019-12-25 13:06:52 +0900

    brewfile を一つにまとめた

--- scripts/brewfile-installer.rb
+++ scripts/brewfile-installer.rb
@@ -2,15 +2,25 @@
 # frozen_string_literal: true
 
 class PackageInstaller
-  def initialize(cmd: ['brew'])
-    @require_packages = []
+  def initialize(cmd: 'brew')
+    @require_packages = {}
     @platform = `uname`.split(/\s+/).first.to_sym.downcase
 
     @cmd = cmd
   end
 
-  def use(*packages)
-    @require_packages += packages.map(&:to_s)
+  def add_with(*subcmd, &block)
+    cmdlist = [@cmd] + subcmd.map(&:to_s)
+    packages = yield block
+    add packages, cmdlist: cmdlist
+  end
+
+  def add(*packages, cmdlist: [@cmd])
+    cmd = cmdlist.join(' ')
+
+    @require_packages[cmd] = [] if @require_packages[cmd].nil?
+
+    @require_packages[cmd] += packages.flatten.map(&:to_s)
   end
 
   def platform?(*platforms)
@@ -18,56 +28,36 @@ class PackageInstaller
   end
 
   def install
-    installed_packages = `#{@cmd.join ' '} list`.split(/\s+/)
-    install_packages = @require_packages.filter do |p|
-      !installed_packages.include? File.basename p
+    @require_packages.each do |cmd, require_packages|
+      installed_packages = `#{cmd} list`.split(/\s+/)
+      install_packages = require_packages.filter do |p|
+        !installed_packages.include? File.basename p
+      end
+
+      next if install_packages.empty?
+
+      install_cmd = cmd.split(/\s/) + ['install'] + install_packages
+      puts install_cmd.join ' '
+      raise unless system(*install_cmd)
     end
-
-    install_cmd = @cmd + ['install'] + install_packages
-
-    return if install_packages.empty?
-
-    puts install_cmd.join ' '
-    raise unless system(*install_cmd)
   end
 
   def upgrade
-    return if @require_packages.empty?
+    @require_packages.each do |cmd, require_packages|
+      next if require_packages.empty?
 
-    upgrade_cmd = @cmd + ['upgrade']
-    puts upgrade_cmd.join ' '
-    raise unless system(*upgrade_cmd)
-  end
-
-  def cleanup
-    cleanup_cmd = @cmd + ['cleanup']
-    puts cleanup_cmd.join ' '
-    raise unless system(*cleanup_cmd)
+      upgrade_cmd = cmd.split(/\s/) + ['upgrade']
+      puts upgrade_cmd.join ' '
+      raise unless system(*upgrade_cmd)
+    end
   end
 end
 
 brew = PackageInstaller.new
 
-%w[
-  brewfile
-  brewfile.local
-].each do |filename|
+Dir.glob("#{__dir__}/brewfile{,\.*}").each do |filename|
   brew.instance_eval File.read filename if File.exist? filename
 end
 
 brew.install
 brew.upgrade
-
-brewcask = PackageInstaller.new(cmd: %w[brew cask])
-
-%w[
-  cask.brewfile
-  cask.brewfile.local
-].each do |filename|
-  brewcask.instance_eval File.read filename if File.exist? filename
-end
-
-brewcask.install
-brewcask.upgrade
-
-brew.cleanup
