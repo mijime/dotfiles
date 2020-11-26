162eed5e66c05ae634d7aca8914d45b159f733f2
mijime <07cc0110+gthb@gmail.com>
2020-11-26 13:36:21 +0900

    Add uniqline cmd

---
+++ gotools/cmd/uniqline/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	if err := uniqline(os.Stdout, os.Stdin); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func uniqline(out io.Writer, in io.Reader) error {
+	sc := bufio.NewScanner(in)
+	mem := make(map[[16]byte]struct{}, 1024)
+
+	for sc.Scan() {
+		b := sc.Bytes()
+		h := md5.Sum(b)
+
+		if _, ok := mem[h]; !ok {
+			mem[h] = struct{}{}
+
+			if _, err := out.Write(b); err != nil {
+				return fmt.Errorf("failed to print: %w", err)
+			}
+
+			if _, err := out.Write([]byte{'\n'}); err != nil {
+				return fmt.Errorf("failed to print: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
